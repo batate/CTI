cb1232ff0e8b6e2e8d7587dcc76da49ee67370f1
Bruce Tate <bruce@rapidred.com>
2014-03-14 20:45:46 -0500

    through basic tests

---
+++ states/test/concrete_test.exs
@@ -0,0 +1,10 @@
+defmodule ConcreteTest do
+  use ExUnit.Case
+  test "should update count" do
+    rented_video = VideoStore.renting(video)
+    assert rented_video.times_rented == 1
+  end
+  
+  def video, do: Video.new title: "XMen"
+  
+end
